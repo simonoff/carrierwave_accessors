5e975fc018bf16cb2a7eda80c4cb99316db9e6b0
Grigory Antonov <antonovga@gmail.com>
2014-12-14 16:37:25 +0300

    + don't break flow for cases without store_accessor

--- lib/carrierwave_accessors/orm/activerecord.rb
+++ lib/carrierwave_accessors/orm/activerecord.rb
@@ -9,7 +9,7 @@ module CarrierWave
       def mount_uploader(column, uploader=nil, options={}, &block)
         super
 
-        ar_store = self.stored_attributes.find{|store, attributes| attributes.include?(column)}.first
+        ar_store = self.stored_attributes.find{|store, attributes| attributes.include?(column)}.try(:first)
         if ar_store
           class_eval <<-RUBY, __FILE__, __LINE__+1
             
@@ -23,7 +23,7 @@ module CarrierWave
             end
         
             def write_uploader(column, identifier)
-              ar_store = self.class.stored_attributes.find{|store, attributes| attributes.include?(column)}.first
+              ar_store = self.class.stored_attributes.find{|store, attributes| attributes.include?(column)}.try(:first)
               if ar_store
                 self.send(ar_store)[column.to_s] = identifier  
               else
@@ -32,7 +32,7 @@ module CarrierWave
             end
 
             def read_uploader(column)
-              ar_store = self.class.stored_attributes.find{|store, attributes| attributes.include?(column)}.first
+              ar_store = self.class.stored_attributes.find{|store, attributes| attributes.include?(column)}.try(:first)
               if ar_store
                 self.send(ar_store)[column.to_s]
               else
